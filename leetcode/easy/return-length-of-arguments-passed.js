c3893c649f41bec408247c40be4a0a8e058963a5
Cee Jay <ceejaymalacas17@gmail.com>
2026-01-05 17:24:08 +0800

    leetcode easy

---
+++ leetcode/easy/return-length-of-arguments-passed.js
@@ -0,0 +1,36 @@
+// Description:
+
+// 2703. Return Length of Arguments Passed
+
+// Write a function argumentsLength that returns the count of arguments passed to it.
+ 
+
+// Example 1:
+
+// Input: args = [5]
+// Output: 1
+// Explanation:
+// argumentsLength(5); // 1
+
+// One value was passed to the function so it should return 1.
+// Example 2:
+
+// Input: args = [{}, null, "3"]
+// Output: 3
+// Explanation: 
+// argumentsLength({}, null, "3"); // 3
+
+// Three values were passed to the function so it should return 3.
+ 
+
+// Constraints:
+
+// args is a valid JSON array
+// 0 <= args.length <= 100
+
+// My Solution:
+var argumentsLength = function(...args) {
+  return args.length
+};
+
+// My Explanation: the ...args syntax allows the function to accept any number of arguments as an array, and then we simply return the length of that array.
